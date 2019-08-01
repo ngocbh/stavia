d87ab8e108aeb0044fa441b83ed4157878cf95be
Ngọc JR <ngocjr7@gmail.com>
2019-08-01 16:05:57 +0700

    off outfile

--- stavia/log_linear_model.py
+++ stavia/log_linear_model.py
@@ -222,9 +222,9 @@ def train():
 
 	data = preprocess(raw_data)
 
-	with codecs.open('new_training_data.json', encoding='utf8', mode='w') as f:
-		jstr = json.dumps(data,ensure_ascii=False, indent=4)
-		f.write(jstr)
+	# with codecs.open('new_training_data.json', encoding='utf8', mode='w') as f:
+	# 	jstr = json.dumps(data,ensure_ascii=False, indent=4)
+	# 	f.write(jstr)
 
 	print('Extracing Feature -----> ')
 	sys.stdout.flush()
@@ -260,13 +260,13 @@ def train():
 			Y_data.append(example_labels)
 		next(status)
 
-	with codecs.open('x.json', encoding='utf8', mode='w') as f:
-		jstr = json.dumps(X_data,ensure_ascii=False, indent=4)
-		f.write(jstr)
+	# with codecs.open('x.json', encoding='utf8', mode='w') as f:
+	# 	jstr = json.dumps(X_data,ensure_ascii=False, indent=4)
+	# 	f.write(jstr)
 
-	with codecs.open('y.json', encoding='utf8', mode='w') as f:
-		jstr = json.dumps(Y_data,ensure_ascii=False, indent=4)
-		f.write(jstr)
+	# with codecs.open('y.json', encoding='utf8', mode='w') as f:
+	# 	jstr = json.dumps(Y_data,ensure_ascii=False, indent=4)
+	# 	f.write(jstr)
 
 	print('Number Positive sample = ', number_positive_sample)
 	print('Number Sample = ', len(Y_data))
